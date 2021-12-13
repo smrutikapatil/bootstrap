eedcf7e8401947fbc905c1cf02a1bbe290ab9d1a
smrutikapatil <smruti.patil17@gmail.com>
2021-12-13 15:18:19 +0530

    har

---
+++ har.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+function harmonicNumber()
+{
+ for((var i=1;i<=15;i++)) {
+if(i==1)
+{
+var addSerias = 1/i;
+}else
+{
+for((var j=2;j<=15;j++))
+{
+if(i==j)
+{
+addSerias=addSerias+"+1"/+i;
+}}}}
+
+}
