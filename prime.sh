62bf862492bf5938d3ccbcba9dbcbeac07c86edf
smrutikapatil <smruti.patil17@gmail.com>
2021-12-13 15:16:32 +0530

    prime

---
+++ prime.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Enter a number" 
+read num
+function prime
+{
+
+for (( i=2; i<=num/2; i++ ))
+do
+
+if [ $((num%i)) -eq 0 ]
+then
+    echo "$num is  not a prime number"
+exit
+fi 
+done
+echo "$n is a prime"
+}
+r=`prime $number`
+echo "$r"
